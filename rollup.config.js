552a27c6a036ffb1532cef33e9aeab3e5a2f7d31
spacegiant <599280+spacegiant@users.noreply.github.com>
2021-11-02 22:45:59 +0000

    Add uglify to build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import { uglify } from 'rollup-plugin-uglify';
 
 
 const isProd = (process.env.BUILD === 'production');
@@ -28,6 +29,7 @@ export default {
     typescript(),
     nodeResolve({browser: true}),
     commonjs(),
-    json()
+    json(),
+    uglify()
   ]
 };
