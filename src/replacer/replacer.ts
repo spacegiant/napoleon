58943d5009d496f3a7b8b5a211719e2353e26837
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-08-21 16:57:22 +0100

    Fix fudge dice

--- src/replacer/replacer.ts
+++ src/replacer/replacer.ts
@@ -1,6 +1,6 @@
 import { MarkdownView, type App } from 'obsidian';
 // https://github.com/GreenImp/rpg-dice-roller
-import { DiceRoll } from 'rpg-dice-roller';
+import { DiceRoll } from '@dice-roller/rpg-dice-roller';
 
 const Replacer = (app: App): void => {
   const view = app.workspace.getActiveViewOfType(MarkdownView);
@@ -16,8 +16,14 @@ const Replacer = (app: App): void => {
   const cleanedLine = currentLine.replace(/\s+/g, ' ').trim();
   const stringList = cleanedLine.split(' ');
   const lastString = stringList[stringList.length - 1];
-  const roll = new DiceRoll(lastString.toLowerCase());
-  stringList[stringList.length - 1] = roll.output;
+
+  try {
+    const roll = new DiceRoll(lastString);
+    stringList[stringList.length - 1] = roll.output;
+  } catch (e) {
+    console.error('Dice notation not recognised');
+  }
+
   const returnString = stringList.join(' ');
   editor.setLine(lineNo, returnString);
   editor.focus();
